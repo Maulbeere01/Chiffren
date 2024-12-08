081a082052eb415faa91176a79b693e9ae9dd158
leetLeach <ben.wehrbergerb@gmail.com>
2024-12-08 18:36:08 +0100

    refactor: improve code readability

--- split_into_n_subarrays.c
+++ split_into_n_subarrays.c
@@ -5,18 +5,18 @@
 #include <stdlib.h>
 // teilt den Geheimtext in so viele Strings wie der Schlüssel lang ist. In die Strings kommen dann jeweils die Buchstaben, die vom gleichen Buchstaben verschlüsselt wurden
 
-char **split_into_n_strings(char *text,int key_Size,const unsigned long int length) {
+char **split_into_subarrays(char *text,int n,const unsigned long int length) {
 
     // wir allokieren ein array mit so vielen Strings wie wir keys haben
-    char **strings = malloc((key_Size * sizeof(char *)));
+    char **strings = malloc((n * sizeof(char *)));
 
     if (strings == NULL) {
         printf("speicherzuweisung Fehler\n");
         return NULL;
     }
     //wir allokieren strings mit passender Größe und speichern sie in dem array strings
-    for (int i = 0; i < key_Size; i++) {
-        strings[i] = malloc(((length / key_Size) + 1) * sizeof(char));
+    for (int i = 0; i < n; i++) {
+        strings[i] = malloc(((length / n) + 1) * sizeof(char));
 
         if (strings[i] == NULL) {
             printf("speicherzuweisung Fehler\n");
@@ -24,10 +24,10 @@ char **split_into_n_strings(char *text,int key_Size,const unsigned long int leng
         }
     }
     // initialisieren der strings, jeweils nur die Buchstaben in einem String, welche mit dem gleichen Buchstaben verschoben wurden
-    for (int i = 0; i < key_Size; i++) {
+    for (int i = 0; i < n; i++) {
 
         int j = 0;
-        for (int k = i; k < length; k += key_Size) { // i bestimmt den String in den geschrieben wird, j bestimmt den Platz des Characters im string,
+        for (int k = i; k < length; k += n) { // i bestimmt den String in den geschrieben wird, j bestimmt den Platz des Characters im string,
             strings[i][j] = text[k];
             j++;
         }
@@ -36,7 +36,7 @@ char **split_into_n_strings(char *text,int key_Size,const unsigned long int leng
     }
 
     printf("\nAusgabe der Strings:\n");
-    for (int i = 0; i < key_Size; i++) {
+    for (int i = 0; i < n; i++) {
         printf("\n%s\n", strings[i]);
     }
 
