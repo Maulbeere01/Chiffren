10211aac200fa5e814f98e116aa456f3526dd93d
ben_framwork_laptop <ben.wehrberger@gmail.com>
2024-12-10 15:41:06 +0100

    chech if cracked

---
+++ check_if_cracked.c
@@ -0,0 +1,14 @@
+//
+// Created by ben10 on 12/9/24.
+//
+
+#include "check_if_cracked.h"
+
+#include <stdio.h>
+
+int check_if_cracked(char *text)
+{
+    FILE *file;
+    file = fopen("words.txt", "r");
+
+}
---
+++ check_if_cracked.h
@@ -0,0 +1,8 @@
+//
+// Created by ben10 on 12/9/24.
+//
+
+#ifndef CHECK_IF_CRACKED_H
+#define CHECK_IF_CRACKED_H
+
+#endif //CHECK_IF_CRACKED_H
